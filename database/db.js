a2dc34fd35b46d4fabd8b4765b89acae576942a7
CarlosHz94 <carlos_h_v18@hotmail.com>
2019-04-14 18:12:20 -0600

    validation

--- database/db.js
+++ database/db.js
@@ -63,6 +63,49 @@ var connectToDatabase = function(){
 	};
 
 	
+var validateUser = function(userName, password){
+	
+	const MongoClient = require('mongodb').MongoClient;
+
+	// replace the uri string with your connection string.
+	const uri = "mongodb+srv://CarlosHz:BlackjackSeng513@blackjack-2j9ms.mongodb.net/test?retryWrites=true";
+	MongoClient.connect(uri, function(err, client) {
+		if(err) {
+			console.log('Error occurred while connecting to MongoDB Atlas...\n',err.stack);
+		}else{
+			console.log('Connected...');
+			const db = client.db("Blackjack");
+			const collection = db.collection("Users");
+			collection.findOne({username: userName}, (function(err, result){
+				if(err){
+					console.log("error in find");
+				}else if(result != null){
+					console.log("success find function");
+					if(result.username === userName && result.password === password){
+						return true;
+					}else{
+						return false;
+					}
+				}else{
+					console.log("user does not exists");
+					return false;
+				}
+			}));
+			client.close();
+			//collection.insertOne( {username: "poop", password: "wtfisthis"});
+			//var query = { username: "poop" };
+			//var newval = { $set: {balance: 500 }};
+			//collection.updateOne(query, newval, function(err, res){
+			//	if(err){
+			//		console.log("error");
+			//	}else{
+			//		console.log("success");
+			//	}
+				// perform actions on the collection object
+				//client.close();
+			}
+		});
+}
 
 var addUser = function(userName){
 	const MongoClient = require('mongodb').MongoClient;
--- public/javascripts/login/login.js
+++ public/javascripts/login/login.js
@@ -1,1 +1,24 @@
-var hi = null;
+
+
+$(document).ready(function(){
+	var socket = io();
+
+	$('#login').click(function(){
+    	login($('#username').val(), $('#password').val());
+  	});
+
+	function login(username, password){
+		console.log("sending " + username + " " + password);
+		socket.emit('login', username, password);
+		socket.on('login success', function(){
+
+		})
+		socket.on('login failure', function(){
+
+		})
+	}
+
+
+
+	
+});
