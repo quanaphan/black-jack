2ada41c02b57469e9cb48f154c3b347a2399beec
brettkaliel <32968043+brettkaliel@users.noreply.github.com>
2019-04-15 16:46:46 -0600

    Added create account and nickneam field

--- public/javascripts/login/login.js
+++ public/javascripts/login/login.js
@@ -1,35 +1,46 @@
+$(document).ready(function () {
+    var socket = io('/login');
+    var usr = ''; //load old cookie
 
+    $('#login').click(function (event) {
+        event.preventDefault();
+        login($('#username').val(), $('#password').val());
+    });
 
-$(document).ready(function(){
-	var socket = io('/login');
-	var usr = ''; //load old cookie
-	$('#login').click(function(event){
-		event.preventDefault();
-    	login($('#username').val(), $('#password').val());
-  	});
+    function login(username, password) {
+        console.log("sending " + username + " " + password);
+        usr = username;
+        socket.emit('login', username, password);
+    }
 
-	function login(username, password){
-		console.log("sending " + username + " " + password);
-		usr = username;
-		socket.emit('login', username, password);
-	}
-
-	socket.on('login success', function(){
-		console.log("success");
-		var now = new Date();
-       	now.setTime(now.getTime() + (24*60*60*1000)); // valid for 1 day
-        cookie_string = 'username=' + usr +'; expires=' + now.toUTCString() + '; path=/';
+    socket.on('login success', function () {
+        console.log("success");
+        var now = new Date();
+        now.setTime(now.getTime() + (24 * 60 * 60 * 1000)); // valid for 1 day
+        cookie_string = 'username=' + usr + '; expires=' + now.toUTCString() + '; path=/';
         document.cookie = cookie_string;
-        cookie_string = 'authorized=' + true +'; expires=' + now.toUTCString() + '; path=/';
+        cookie_string = 'authorized=' + true + '; expires=' + now.toUTCString() + '; path=/';
         document.cookie = cookie_string;
-		window.location.href = "/lobby";
+        window.location.href = "/lobby";
 
-	});
-	socket.on('login failure', function(){
-		console.log("failed");
-	});
+    });
+    socket.on('login failure', function () {
+        console.log("failed");
+    });
+
+    $('#create-account').prop('disabled', true);
 
 
+    $('#new-password, #check-new-password').on('keyup', function () {
 
-	
+        if ($('#new-password').val() == $('#check-new-password').val()) {
+            $('#matching-message').html('Matching').css('color', 'green');
+            $('#create-account').prop('disabled', false);
+        } else {
+            $('#matching-message').html('Not Matching').css('color', 'red');
+            $('#create-account').prop('disabled', true);
+        }
+    });
+
+
 });
