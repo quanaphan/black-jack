564b65f96b3d4af714cfcdc29aa18015cbeb762c
brettkaliel <32968043+brettkaliel@users.noreply.github.com>
2019-04-15 21:28:45 -0600

    Join game Button

--- public/javascripts/lobby/lobby.js
+++ public/javascripts/lobby/lobby.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	var socket = io('/lobby');
 
 	var player = {};
-	socket.on('init', function(sessionPlayer){
+	socket.on('init', function (sessionPlayer) {
 		console.log("inside init");
 		player = JSON.parse(sessionPlayer);
 		console.log(player);
@@ -10,78 +10,85 @@ $(document).ready(function(){
 		$('#money').text("Balance: $" + player['balance']);
 	});
 
-	$('#game-create-enter').click(function(event){
+	$('#game-create-enter').click(function (event) {
 		event.preventDefault();
 		host($('#room-name').val());
 	});
 
-	function host(roomName){
+	function host(roomName) {
 		console.log("creating room");
 		socket.emit('create room', roomName);
 	}
 
 	var gameSessions = {};
-	socket.on('lobby update', function(game_sessions){
+	socket.on('lobby update', function (game_sessions) {
 		console.log("lobby updated");
 		gameSessions = JSON.parse(game_sessions);
 		console.log(gameSessions);
 		$('#gamerooms').empty();
 		var count = Object.keys(gameSessions).length;
 		var i;
-		for(i = 0; i < count; i++){
-			if(gameSessions[i]['active']){
-				$('#gamerooms').append($('<li>').text("Room#" + gameSessions[i]['id'] + " \"" + gameSessions[i]['title'] + "\" " + gameSessions[i]['capacity'] + "/3"));
+		for (i = 0; i < count; i++) {
+			if (gameSessions[i]['active']) {
+				$('#gamerooms').append("<li>" + "Room #" + gameSessions[i]['id'] + " \"" + gameSessions[i]['title'] + "\" " + gameSessions[i]['capacity'] + "/3" + "</li>");
 			}
 		}
 
 	});
 
 
-	socket.on('redirect to login', function(){
+	socket.on('redirect to login', function () {
 		console.log("redirecting to login");
 		document.cookie = "username= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 		document.cookie = "authorized= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
 		window.location.href = "/";
 	});
 
-	socket.on('join fail', function(){
+	socket.on('join fail', function () {
 		//TODO logic for when trying to join room that is full
 		console.log("join failed");
 	});
 
-	$('#logout').click(function(event){
+	$('#logout').click(function (event) {
 		event.preventDefault();
 		logout();
 	});
 
-	function logout(){
+	function logout() {
 		console.log("logging out");
 		document.cookie = "username= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
 		document.cookie = "authorized= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
 		window.location.href = "/";
 	}
 
-	$('#refresh').click(function(event){
+	$('#refresh').click(function (event) {
 		event.preventDefault();
 		refresh();
 	});
 
-	function refresh(){
+	function refresh() {
 		console.log("refresh");
 		socket.emit('refresh');
 	}
 
-	$('#account').click(function(event){
+	$('#account').click(function (event) {
 		event.preventDefault();
 		account();
 	});
 
-	function account(){
+	function account() {
 		window.location.href = "/account";
 	}
-	
+
 	$('#game-create-enter').click(function () {
-        let roomName = $('#newRoomModal #room-name').val();
-        $('#newRoomModal #room-name').val("");
-    });
+		let roomName = $('#newRoomModal #room-name').val();
+		$('#newRoomModal #room-name').val("");
+	});
+
+
+
+
+
+
 });
+
